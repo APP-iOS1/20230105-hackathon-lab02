799ff0ad0e5e4fb8441767c0643aef42c6bcd4a9
yooj <114331071+yooj1202@users.noreply.github.com>
2023-01-06 02:34:36 +0900

    feat: add Admin View

--- AwesomeKoreanDictionary/AwesomeKoreanDictionary/View/MyPageView/MyPageView_SignOut.swift
+++ AwesomeKoreanDictionary/AwesomeKoreanDictionary/View/MyPageView/MyPageView_SignOut.swift
@@ -80,7 +80,7 @@ struct MyPageView_SignOut: View {
                                 .padding(.top)
                             
                             NavigationLink{
-//                                MyPageView_MasterLoginView()
+                                AdminMainView()
                             } label : {
                             Text(thirdMyPageList[0])
                                 .padding(.horizontal)
