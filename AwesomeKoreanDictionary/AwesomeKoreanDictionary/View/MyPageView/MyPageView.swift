9ca921e0c01685a67b8709b4f890effdef6eff8b
yooj <114331071+yooj1202@users.noreply.github.com>
2023-01-05 22:11:29 +0900

    feat: 뷰파일 누락건

---
+++ AwesomeKoreanDictionary/AwesomeKoreanDictionary/View/MyPageView/MyPageView.swift
@@ -0,0 +1,28 @@
+//
+//  MyPageView1.swift
+//  AwesomeKoreanDictionary
+//
+//  Created by Yooj on 2023/01/05.
+//
+
+import SwiftUI
+
+struct MyPageView: View {
+    @EnvironmentObject var authManager: AuthManager
+    
+    
+    
+    var body: some View {
+        if authManager.state == .signedIn {
+            MyPageView_SignIn()
+        } else {
+            MyPageView_SignOut()
+        }
+    }
+}
+
+struct MyPageView1_Previews: PreviewProvider {
+    static var previews: some View {
+        MyPageView()
+    }
+}
