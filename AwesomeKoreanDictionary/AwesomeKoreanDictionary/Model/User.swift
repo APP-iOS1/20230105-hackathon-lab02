b2627b67a6ada469ac8250fd31e2a62f31f76ece
Choo Hyun Ho <hyunho4178@gmail.com>
2023-01-05 14:29:16 +0900

    [Chore] vocab CRU(D) updated
    [Chore] vocab CRU(D) updated

---
+++ AwesomeKoreanDictionary/AwesomeKoreanDictionary/Model/User.swift
@@ -0,0 +1,16 @@
+//
+//  User.swift
+//  AwesomeKoreanDictionary
+//
+//  Created by 추현호 on 2023/01/05.
+//
+
+import Foundation
+
+struct User {
+    var isAdmin: Bool
+    var userNickname: String
+    var bookmarked: [String]
+    var createdVoca: [String]
+    var userId: String
+}
---
+++ AwesomeKoreanDictionary/AwesomeKoreanDictionary/Store/AuthManager.swift
@@ -0,0 +1,9 @@
+//
+//  AuthManager.swift
+//  AwesomeKoreanDictionary
+//
+//  Created by 추현호 on 2023/01/05.
+//
+
+import FirebaseAuth
+import FirebaseFirestore
