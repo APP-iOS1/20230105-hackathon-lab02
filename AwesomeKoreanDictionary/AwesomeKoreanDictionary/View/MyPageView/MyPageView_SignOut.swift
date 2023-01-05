449b521771e36b0810101ece7d3301cab4f59260
zoohee <jjhh1545@gmail.com>
2023-01-05 16:47:24 +0900

    [Add] RegistrationView

--- AwesomeKoreanDictionary/AwesomeKoreanDictionary/View/MyPageView/MyPageView_SignOut.swift
+++ AwesomeKoreanDictionary/AwesomeKoreanDictionary/View/MyPageView/MyPageView_SignOut.swift
@@ -71,7 +71,7 @@ struct MyPageView_SignOut: View {
                                 .padding(.top)
                             
                             NavigationLink{
-                                MyPageView_MasterLoginView()
+//                                MyPageView_MasterLoginView()
                             } label : {
                             Text(thirdMyPageList[0])
                                 .padding(.horizontal)
