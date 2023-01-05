4c1101554b5ae8f11a20877e67e01d2466d47118
zoohee <jjhh1545@gmail.com>
2023-01-05 21:18:15 +0900

    [Chore] SlangRegistrationView 코드 수정

--- AwesomeKoreanDictionary/AwesomeKoreanDictionary/View/RegistraionView/SlangRegistrationView.swift
+++ AwesomeKoreanDictionary/AwesomeKoreanDictionary/View/RegistraionView/SlangRegistrationView.swift
@@ -32,18 +32,16 @@ struct SlangRegistrationView: View {
     private var trimslangDescriptionTextField: String {
         slangDescriptionTextField.trimmingCharacters(in: .whitespaces)
     }
+    //속어 상황 재연 텍스트필드 공백체크
+    private var trimslangSituationUsedTextField: String {
+        slangSituationUsedTextField.trimmingCharacters(in: .whitespaces)
+    }
     
-    //MARK: - 국적 선택 시 사용되는 속성들
-    let nationalityOptions: [String] = ["korean","english","japanese"]
-    
-    @State private (set) var selectednationalityIndex: Int = 0  // 국적 피커 전용 인덱스 (91번째 코드에 사용 됨)
-    @State private (set) var selectednationality: String = ""   // 유저가 선택한 국적
     @State private var summitAlertToggle: Bool = false          // 공백문자 없으면 띄우는 얼럿
     @State private var summitAlertToggle2: Bool = false         // 공백문자만 있으면 띄우는 얼럿
     
     var body: some View {
         NavigationView {
-            
             VStack{
                 //MARK: - 텍스트필드 3개 (필수2, 선택1) + 국적선택 피커 1개
                 Form{
@@ -54,8 +52,10 @@ struct SlangRegistrationView: View {
                             .font(.caption)
                             .foregroundColor(.red)
                         TextField("속어를 입력해주세요.", text: $slangTextField)
-                            .font(.subheadline)
-                            .padding([.top, .bottom], 30)
+                            .font(.title3)
+                            .fontWeight(.bold)
+                            .lineLimit(1, reservesSpace: true)
+                            .frame(width: 320, height: 30, alignment: .top)
                         Text("예) 농협은행")
                             .font(.caption)
                             .foregroundColor(.gray)
@@ -65,9 +65,11 @@ struct SlangRegistrationView: View {
                         Text("*필수 (공백만 입력 불가능)")
                             .font(.caption)
                             .foregroundColor(.red)
-                        TextField("속어에 사용되는 단어의 의미를 설명해주세요.", text: $slangDescriptionTextField)
+                        TextField("속어에 사용되는 단어의 의미를 설명해주세요.", text: $slangDescriptionTextField, axis: .vertical)
                             .font(.subheadline)
-                            .padding([.top, .bottom],70)
+                            .lineLimit(7, reservesSpace: true)
+                            .frame(width: 320, height: 150, alignment: .top)
+
                         Text("예) \(DescriptionExample)")
                             .font(.caption)
                         
@@ -75,12 +77,13 @@ struct SlangRegistrationView: View {
                     }
                     //속어가 사용되는 상활 재연(선택)
                     Section{
-                        Text("*선택")
+                        Text("*필수 (공백만 입력 불가능)")
                             .font(.caption)
-                            .foregroundColor(.blue)
-                        TextField("속어가 사용되는 상황을 재미있게 공유해보세요!", text: $slangSituationUsedTextField)
+                            .foregroundColor(.red)
+                        TextField("속어가 사용되는 상황을 재미있게 공유해보세요!", text: $slangSituationUsedTextField, axis: .vertical)
                             .font(.subheadline)
-                            .padding([.top, .bottom],70)
+                            .lineLimit(7, reservesSpace: true)
+                            .frame(width: 320, height: 150, alignment: .top)
                         Text("예) \(SituationUsedExample)")
                             .font(.caption)
                         
@@ -88,26 +91,13 @@ struct SlangRegistrationView: View {
                         
                         
                     }
-                    //국적 선택 피커
-                    //                    Section{
-                    //                        Picker("국적", selection: $selectednationalityIndex) {
-                    //                            ForEach(0..<nationalityOptions.count, id:\.self) { option in
-                    //                                Text("\(nationalityOptions[option])")
-                    //                            }
-                    //                            .onChange(of: selectednationalityIndex) { newValue in
-                    //                                self.selectednationality = nationalityOptions[selectednationalityIndex]
-                    //                            }
-                    //                        }.pickerStyle(.menu)
-                    //                    }
-                    
                 }
                 .padding(.bottom)
                 
                 //MARK: - 제출하기 버튼
-                if trimslangTextField.count > 0 && trimslangDescriptionTextField.count > 0{
+                if trimslangTextField.count > 0 && trimslangDescriptionTextField.count > 0 && trimslangSituationUsedTextField.count > 0 {
                     VStack{
                         Button {
-                            // TODO: createVoca 추가하기
                             Task {
                                 await vocaManager.createVoca(voca: Vocabulary(id: UUID().uuidString, word: slangTextField, pronunciation: "", definition: slangDescriptionTextField, example: [slangSituationUsedTextField], likes: 0, dislikes: 0, creatorId: Auth.auth().currentUser?.uid ?? ""))
                                 
@@ -136,14 +126,13 @@ struct SlangRegistrationView: View {
                         } message: {
                             Text(" 마이페이지에서 승인여부 확인 가능합니다. ")
                         }
-                    }// 버튼 VStack
+                    }
                 } else {
                     //공백문자만 입력시 얼럿창 띄우는 코드
                     VStack{
                         Button(action: {
                             summitAlertToggle2.toggle()
                         }) {
-                            
                             ZStack{
                                 RoundedRectangle(cornerRadius: 10)
                                     .fill(Color.clear)
@@ -158,41 +147,36 @@ struct SlangRegistrationView: View {
                                             .font(.title3)
                                     }
                             }
-                            
                         }
                         .alert("필수문항을 모두 입력해주세요.", isPresented: $summitAlertToggle2) {
                             Button("Ok") {}
                         }
-                    }// 버튼 VStack
+                    }
                 }
-                
-            }//다 감싸고있는 VStack
+            }
         }
     }
 }
+
 extension SlangRegistrationView {
     //간단한 등록 설명 텍스트 (프리뷰 맨 위에 사용 됨)
     private var descriptionText: some View {
         VStack{
             HStack{
-                VStack(alignment: .leading, spacing: 5){
-                    
+                VStack(alignment: .leading, spacing: 5) {
                     Text("AwesomeKoreanDictionary 에서 자신만의 단어 뜻을 넣어서 공유해보세요!")
-                    
                 }
                 .font(.subheadline)
                 Spacer()
             }
             HStack{
-                VStack(alignment: .leading, spacing: 5){
-                    
+                VStack(alignment: .leading, spacing: 5) {
                     Text("많은 사람들이 보기 좋게 써주세요.")
                     Text("많은 사람들이 이 뜻을 읽고 궁금해할 수 있으니 작은 뒷 이야기도 함께 써주세요.")
                     Text("일반적으로 사용되고있는 단어의 뜻만 써주세요.")
                     Text("(개인적인 농담과 관련된 단어의 뜻은 거부 할 것입니다.)")
                         .font(.caption)
                         .foregroundColor(.blue)
-                    
                 }
                 .font(.subheadline)
                 Spacer()
