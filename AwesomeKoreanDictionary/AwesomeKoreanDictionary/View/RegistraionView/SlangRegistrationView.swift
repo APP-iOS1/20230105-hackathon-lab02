2eb4ab51407f1f451a34aed69072cb46ec18244d
Choo Hyun Ho <hyunho4178@gmail.com>
2023-01-06 06:19:59 +0900

    [Chore] dissmiss after signin In RegisterView
    [Chore] dissmiss after signin In RegisterView

--- AwesomeKoreanDictionary/AwesomeKoreanDictionary/View/RegistraionView/SlangRegistrationView.swift
+++ AwesomeKoreanDictionary/AwesomeKoreanDictionary/View/RegistraionView/SlangRegistrationView.swift
@@ -7,9 +7,11 @@
 
 import SwiftUI
 import FirebaseAuth
+import GoogleSignIn
 
 struct SlangRegistrationView: View {
     @ObservedObject var vocaManager: VocabularyNetworkManager = VocabularyNetworkManager()
+    @EnvironmentObject var authManager: AuthManager
     @Environment(\.dismiss) private var dismiss
     
     //MARK: - 텍스트필드 작성시 사용되는 속성들
@@ -43,115 +45,119 @@ struct SlangRegistrationView: View {
     var body: some View {
         NavigationView {
             VStack {
-                //MARK: - 텍스트필드 3개 (필수2, 선택1) + 국적선택 피커 1개
-                Form {
-                    descriptionText
-                    //속어 입력 텍스트필드(필수)
-                    Section {
-                        Text("*필수 (공백만 입력 불가능)")
-                            .font(.caption)
-                            .foregroundColor(.red)
-                        TextField("속어를 입력해주세요.", text: $slangTextField)
-                            .font(.title3)
-                            .fontWeight(.bold)
-                            .lineLimit(1, reservesSpace: true)
-                            .frame(width: 320, height: 30, alignment: .top)
-                        Text("예) 농협은행")
-                            .font(.caption)
-                            .foregroundColor(.gray)
+                if authManager.state == .signedIn {
+                    //MARK: - 텍스트필드 3개 (필수2, 선택1) + 국적선택 피커 1개
+                    Form {
+                        descriptionText
+                        //속어 입력 텍스트필드(필수)
+                        Section {
+                            Text("*필수 (공백만 입력 불가능)")
+                                .font(.caption)
+                                .foregroundColor(.red)
+                            TextField("속어를 입력해주세요.", text: $slangTextField)
+                                .font(.title3)
+                                .fontWeight(.bold)
+                                .lineLimit(1, reservesSpace: true)
+                                .frame(width: 320, height: 30, alignment: .top)
+                            Text("예) 농협은행")
+                                .font(.caption)
+                                .foregroundColor(.gray)
+                        }
+                        //속어에 사용되는 단어 의미 설명 텍스트필드(필수)
+                        Section {
+                            Text("*필수 (공백만 입력 불가능)")
+                                .font(.caption)
+                                .foregroundColor(.red)
+                            TextField("속어에 사용되는 단어의 의미를 설명해주세요.", text: $slangDescriptionTextField, axis: .vertical)
+                                .font(.subheadline)
+                                .lineLimit(7, reservesSpace: true)
+                                .frame(width: 320, height: 150, alignment: .top)
+                            
+                            Text("예) \(DescriptionExample)")
+                                .font(.caption)
+                            
+                                .foregroundColor(.gray)
+                        }
+                        //속어가 사용되는 상활 재연(선택)
+                        Section {
+                            Text("*필수 (공백만 입력 불가능)")
+                                .font(.caption)
+                                .foregroundColor(.red)
+                            TextField("속어가 사용되는 상황을 재미있게 공유해보세요!", text: $slangSituationUsedTextField, axis: .vertical)
+                                .font(.subheadline)
+                                .lineLimit(7, reservesSpace: true)
+                                .frame(width: 320, height: 150, alignment: .top)
+                            Text("예) \(SituationUsedExample)")
+                                .font(.caption)
+                            
+                                .foregroundColor(.gray)
+                            
+                            
+                        }
                     }
-                    //속어에 사용되는 단어 의미 설명 텍스트필드(필수)
-                    Section {
-                        Text("*필수 (공백만 입력 불가능)")
-                            .font(.caption)
-                            .foregroundColor(.red)
-                        TextField("속어에 사용되는 단어의 의미를 설명해주세요.", text: $slangDescriptionTextField, axis: .vertical)
-                            .font(.subheadline)
-                            .lineLimit(7, reservesSpace: true)
-                            .frame(width: 320, height: 150, alignment: .top)
-                        
-                        Text("예) \(DescriptionExample)")
-                            .font(.caption)
-                        
-                            .foregroundColor(.gray)
-                    }
-                    //속어가 사용되는 상활 재연(선택)
-                    Section {
-                        Text("*필수 (공백만 입력 불가능)")
-                            .font(.caption)
-                            .foregroundColor(.red)
-                        TextField("속어가 사용되는 상황을 재미있게 공유해보세요!", text: $slangSituationUsedTextField, axis: .vertical)
-                            .font(.subheadline)
-                            .lineLimit(7, reservesSpace: true)
-                            .frame(width: 320, height: 150, alignment: .top)
-                        Text("예) \(SituationUsedExample)")
-                            .font(.caption)
-                        
-                            .foregroundColor(.gray)
-                        
-                        
-                    }
-                }
-                .padding(.bottom)
-                
-                //MARK: - 제출하기 버튼
-                if trimslangTextField.count > 0 && trimslangDescriptionTextField.count > 0 && trimslangSituationUsedTextField.count > 0 {
-                    VStack {
-                        Button {
-                            Task {
-                                await vocaManager.createVoca(voca: Vocabulary(id: UUID().uuidString, word: slangTextField, pronunciation: "", definition: slangDescriptionTextField, example: slangSituationUsedTextField, likes: 0, dislikes: 0, creatorId: Auth.auth().currentUser?.uid ?? ""))
-                                
-                                summitAlertToggle.toggle()
-                                
-                                print(Vocabulary(id: UUID().uuidString, word: slangTextField, pronunciation: "", definition: slangDescriptionTextField, example: slangSituationUsedTextField, likes: 0, dislikes: 0, creatorId: Auth.auth().currentUser?.uid ?? ""))
+                    .padding(.bottom)
+                    
+                    //MARK: - 제출하기 버튼
+                    if trimslangTextField.count > 0 && trimslangDescriptionTextField.count > 0 && trimslangSituationUsedTextField.count > 0 {
+                        VStack {
+                            Button {
+                                Task {
+                                    await vocaManager.createVoca(voca: Vocabulary(id: UUID().uuidString, word: slangTextField, pronunciation: "", definition: slangDescriptionTextField, example: slangSituationUsedTextField, likes: 0, dislikes: 0, creatorId: Auth.auth().currentUser?.uid ?? ""))
+                                    
+                                    summitAlertToggle.toggle()
+                                    
+                                    print(Vocabulary(id: UUID().uuidString, word: slangTextField, pronunciation: "", definition: slangDescriptionTextField, example: slangSituationUsedTextField, likes: 0, dislikes: 0, creatorId: Auth.auth().currentUser?.uid ?? ""))
+                                }
+                            } label: {
+                                ZStack {
+                                    RoundedRectangle(cornerRadius: 10)
+                                        .fill(Color.clear)
+                                        .frame(width: 350, height: 70)
+                                    RoundedRectangle(cornerRadius: 10)
+                                        .fill(Color.green)
+                                        .frame(width: 350, height: 60)
+                                        .overlay {
+                                            Text("제출하기")
+                                                .foregroundColor(.white)
+                                                .fontWeight(.black)
+                                                .font(.title3)
+                                        }
+                                }
                             }
-                        } label: {
-                            ZStack {
-                                RoundedRectangle(cornerRadius: 10)
-                                    .fill(Color.clear)
-                                    .frame(width: 350, height: 70)
-                                RoundedRectangle(cornerRadius: 10)
-                                    .fill(Color.green)
-                                    .frame(width: 350, height: 60)
-                                    .overlay {
-                                        Text("제출하기")
-                                            .foregroundColor(.white)
-                                            .fontWeight(.black)
-                                            .font(.title3)
-                                    }
+                            .alert("공유해주셔서 감사합니다!", isPresented: $summitAlertToggle) {
+                                Button("Ok") { dismiss() }
+                            } message: {
+                                Text(" 마이페이지에서 승인여부 확인 가능합니다. ")
                             }
                         }
-                        .alert("공유해주셔서 감사합니다!", isPresented: $summitAlertToggle) {
-                            Button("Ok") { dismiss() }
-                        } message: {
-                            Text(" 마이페이지에서 승인여부 확인 가능합니다. ")
+                    } else {
+                        //공백문자만 입력시 얼럿창 띄우는 코드
+                        VStack {
+                            Button(action: {
+                                summitAlertToggle2.toggle()
+                            }) {
+                                ZStack {
+                                    RoundedRectangle(cornerRadius: 10)
+                                        .fill(Color.clear)
+                                        .frame(width: 350, height: 70)
+                                    RoundedRectangle(cornerRadius: 10)
+                                        .fill(Color.red)
+                                        .frame(width: 350, height: 60)
+                                        .overlay {
+                                            Text("제출하기")
+                                                .foregroundColor(.white)
+                                                .fontWeight(.black)
+                                                .font(.title3)
+                                        }
+                                }
+                            }
+                            .alert("필수문항을 모두 입력해주세요.", isPresented: $summitAlertToggle2) {
+                                Button("Ok") { }
+                            }
                         }
                     }
                 } else {
-                    //공백문자만 입력시 얼럿창 띄우는 코드
-                    VStack {
-                        Button(action: {
-                            summitAlertToggle2.toggle()
-                        }) {
-                            ZStack {
-                                RoundedRectangle(cornerRadius: 10)
-                                    .fill(Color.clear)
-                                    .frame(width: 350, height: 70)
-                                RoundedRectangle(cornerRadius: 10)
-                                    .fill(Color.red)
-                                    .frame(width: 350, height: 60)
-                                    .overlay {
-                                        Text("제출하기")
-                                            .foregroundColor(.white)
-                                            .fontWeight(.black)
-                                            .font(.title3)
-                                    }
-                            }
-                        }
-                        .alert("필수문항을 모두 입력해주세요.", isPresented: $summitAlertToggle2) {
-                            Button("Ok") { }
-                        }
-                    }
+                    LoginView()
                 }
             }
         }
