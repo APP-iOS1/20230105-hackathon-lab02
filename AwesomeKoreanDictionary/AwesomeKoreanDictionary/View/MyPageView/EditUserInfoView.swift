8b0a5d1013cf9239be1dda446e9af8c63475e2e6
Primrose1101 <114223237+Primrose1101@users.noreply.github.com>
2023-01-18 01:53:28 +0900

    HIG 디자인 적용_1차

--- AwesomeKoreanDictionary/AwesomeKoreanDictionary/View/MyPageView/EditUserInfoView.swift
+++ AwesomeKoreanDictionary/AwesomeKoreanDictionary/View/MyPageView/EditUserInfoView.swift
@@ -41,6 +41,7 @@ struct EditUserInfoView: View {
                         .foregroundColor(.gray)
                         .background(Color.white)
                         .frame(width: 350, height: 65)
+                        .cornerRadius(10)
                     
                 }
                 .padding(.bottom, 20)
