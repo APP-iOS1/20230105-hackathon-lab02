263f34abaa05d8f98a41057f786c71491090459f
parkjekyun@gmail.com <parkjekyun@gmail.com>
2023-01-20 00:37:08 +0900

    [Add] 계정삭제시 Firebase Authentication에서 제거하기 위한 deleteUser 함수 추가

--- AwesomeKoreanDictionary/AwesomeKoreanDictionary/ViewModel/AuthManager.swift
+++ AwesomeKoreanDictionary/AwesomeKoreanDictionary/ViewModel/AuthManager.swift
@@ -15,7 +15,7 @@ import SwiftUI
 
 final class AuthManager: ObservableObject {
     @Published var nonce = ""
-    
+
     enum signInState {
         case signedIn
         case signedOut
@@ -25,15 +25,15 @@ final class AuthManager: ObservableObject {
     @Published var currentUser = GIDSignIn.sharedInstance.currentUser
     @Published var user: User = User(id: "", isAdmin: false, userNickname: "", userEmail: "")
     @Published var currentUserInfo: User = User(id: "", isAdmin: false, userNickname: "", userEmail: "")
-    
+
     func signIn() {
         if GIDSignIn.sharedInstance.hasPreviousSignIn() {
             GIDSignIn.sharedInstance.restorePreviousSignIn { [unowned self] user, error in
-                
+
                 let db = Firestore.firestore()
                 let currentUserID = user?.userID ?? ""
                 let userRef = db.collection("user").document(currentUserID)
-                
+
                 userRef.getDocument {
                     (document, error) in
                     if document!.exists {
@@ -42,7 +42,7 @@ final class AuthManager: ObservableObject {
                         let isAdmin: Bool = docData?["isAdmin"] as? Bool ?? false
                         let userEmail: String = docData?["userEmail"] as? String ?? ""
                         let userNickname: String = docData?["userNickname"] as? String ?? ""
-                        
+
                         self.currentUserInfo.id = id
                         self.currentUserInfo.isAdmin = isAdmin
                         self.currentUserInfo.userEmail = userEmail
@@ -53,29 +53,29 @@ final class AuthManager: ObservableObject {
                             "isAdmin": false,
                             "userEmail": user?.profile?.email ?? "",
                             "userNickname": user?.profile?.name ?? ""
-                        ], merge: true)
-                        
+                            ], merge: true)
+
                         self.currentUserInfo.id = currentUserID
                         self.currentUserInfo.isAdmin = false
                         self.currentUserInfo.userEmail = user?.profile?.email ?? ""
                         self.currentUserInfo.userNickname = user?.profile?.name ?? ""
                     }
                 }
-                
+
                 authenticateUser(for: user, with: error)
             }
         } else {
             guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene else { return }
             guard let rootViewController = windowScene.windows.first?.rootViewController else { return }
-            
+
             GIDSignIn.sharedInstance.signIn(withPresenting: rootViewController) { result, error in
                 if let googleUser = result?.user {
-                    
+
                     let db = Firestore.firestore()
                     let currentUserID = googleUser.userID ?? ""
                     let userRef = db.collection("user").document(currentUserID)
                     print(currentUserID)
-                    
+
                     userRef.getDocument {
                         (document, error) in
                         if document!.exists {
@@ -84,7 +84,7 @@ final class AuthManager: ObservableObject {
                             let isAdmin: Bool = docData?["isAdmin"] as? Bool ?? false
                             let userEmail: String = docData?["userEmail"] as? String ?? ""
                             let userNickname: String = docData?["userNickname"] as? String ?? ""
-                            
+
                             self.currentUserInfo.id = id
                             self.currentUserInfo.isAdmin = isAdmin
                             self.currentUserInfo.userEmail = userEmail
@@ -95,15 +95,15 @@ final class AuthManager: ObservableObject {
                                 "isAdmin": false,
                                 "userEmail": googleUser.profile?.email ?? "",
                                 "userNickname": googleUser.profile?.name ?? ""
-                            ], merge: true)
-                            
+                                ], merge: true)
+
                             self.currentUserInfo.id = currentUserID
                             self.currentUserInfo.isAdmin = false
                             self.currentUserInfo.userEmail = googleUser.profile?.email ?? ""
                             self.currentUserInfo.userNickname = googleUser.profile?.name ?? ""
                         }
                     }
-                    
+
                     self.currentUserInfo.id = currentUserID
                     self.currentUserInfo.isAdmin = false
                     self.currentUserInfo.userEmail = googleUser.profile?.email ?? ""
@@ -113,29 +113,29 @@ final class AuthManager: ObservableObject {
             }
         }
     }
-    
+
     private func authenticateUser(for user: GIDGoogleUser?, with error: Error?) {
         if let error = error {
-#if DEBUG
-            print("\(error.localizedDescription)")
-#endif
+            #if DEBUG
+                print("\(error.localizedDescription)")
+            #endif
             return
         }
         guard let authenticationToken = user?.accessToken.tokenString, let idToken = user?.idToken?.tokenString else { return }
-        
+
         let credential = GoogleAuthProvider.credential(withIDToken: idToken, accessToken: authenticationToken)
-        
+
         Auth.auth().signIn(with: credential) { [unowned self] (_, error) in
             if let error = error {
-#if DEBUG
-                print("\(error.localizedDescription)")
-#endif
+                #if DEBUG
+                    print("\(error.localizedDescription)")
+                #endif
             } else {
                 self.state = .signedIn
             }
         }
     }
-    
+
     // MARK: SIGN OUT
     func signOut() {
         GIDSignIn.sharedInstance.signOut()
@@ -144,37 +144,54 @@ final class AuthManager: ObservableObject {
             state = .signedOut
             UserInfoManager().userInfo = nil
         } catch {
-#if DEBUG
-            print("\(error.localizedDescription)")
-#endif
+            #if DEBUG
+                print("\(error.localizedDescription)")
+            #endif
         }
     }
-    
+    // MARK: - Delete User
+
+    func deleteUser() {
+        let user = Auth.auth().currentUser
+
+        user?.delete { error in
+            if let error = error {
+                #if DEBUG
+                    print("\(error.localizedDescription)")
+                #endif
+            } else {
+                #if DEBUG
+                    print("user deleted successfully")
+                #endif
+            }
+        }
+        self.state = .signedOut
+    }
+
+
+
     //MARK: - Apple log-in
-    
-    
-    
-    
-    func authenticate(credential: ASAuthorizationAppleIDCredential){
-        
-        guard let token = credential.identityToken else{
+
+    func authenticate(credential: ASAuthorizationAppleIDCredential) {
+
+        guard let token = credential.identityToken else {
             print("error with firebase")
-            
+
             return
         }
-        
-        guard let tokenString = String(data: token, encoding: .utf8) else{
+
+        guard let tokenString = String(data: token, encoding: .utf8) else {
             print("error with Token")
             return
         }
-        
+
         let firebaseCredential = OAuthProvider.credential(withProviderID: "apple.com", idToken: tokenString, rawNonce: nonce)
-        
+
         Auth.auth().signIn(with: firebaseCredential) { (result, err) in
             let db = Firestore.firestore()
             let currentUserID = result?.user.uid ?? ""
             let userRef = db.collection("user").document(currentUserID)
-            
+
             userRef.getDocument {
                 (document, error) in
                 if document!.exists {
@@ -183,7 +200,7 @@ final class AuthManager: ObservableObject {
                     let isAdmin: Bool = docData?["isAdmin"] as? Bool ?? false
                     let userEmail: String = docData?["userEmail"] as? String ?? ""
                     let userNickname: String = docData?["userNickname"] as? String ?? ""
-                    
+
                     self.currentUserInfo.id = id
                     self.currentUserInfo.isAdmin = isAdmin
                     self.currentUserInfo.userEmail = userEmail
@@ -194,26 +211,26 @@ final class AuthManager: ObservableObject {
                         "isAdmin": false,
                         "userEmail": result?.user.email ?? "",
                         "userNickname": result?.user.email ?? ""
-                    ], merge: true)
-                    
+                        ], merge: true)
+
                     self.currentUserInfo.id = currentUserID
                     self.currentUserInfo.isAdmin = false
                     self.currentUserInfo.userEmail = result?.user.email ?? ""
                     self.currentUserInfo.userNickname = result?.user.email ?? ""
                 }
             }
-            
-            if let error = err{
-#if DEBUG
-                print("\(error.localizedDescription)")
-#endif
+
+            if let error = err {
+                #if DEBUG
+                    print("\(error.localizedDescription)")
+                #endif
                 return
             }
         }
-#if DEBUG
-        print("Logged In")
-#endif
-        withAnimation(.easeInOut){
+        #if DEBUG
+            print("Logged In")
+        #endif
+        withAnimation(.easeInOut) {
             self.state = .signedIn
         }
     }
@@ -225,17 +242,17 @@ func sha256(_ input: String) -> String {
     let hashString = hashedData.compactMap {
         return String(format: "%02x", $0)
     }.joined()
-    
+
     return hashString
 }
 
 func randomNonceString(length: Int = 32) -> String {
     precondition(length > 0)
     let charset: [Character] =
-    Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
+        Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
     var result = ""
     var remainingLength = length
-    
+
     while remainingLength > 0 {
         let randoms: [UInt8] = (0 ..< 16).map { _ in
             var random: UInt8 = 0
@@ -247,12 +264,12 @@ func randomNonceString(length: Int = 32) -> String {
             }
             return random
         }
-        
+
         randoms.forEach { random in
             if remainingLength == 0 {
                 return
             }
-            
+
             if random < charset.count {
                 result.append(charset[Int(random)])
                 remainingLength -= 1
